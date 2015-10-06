f78752ba616a9f3cff63df8b3d17d96d2bae6400
Danila Dergachev <gnomdan@yandex.ru>
2015-10-06 12:02:59 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('error', function (error) {
+    console.error(error);
+});
+
+function getMessage() {
+    this.cnt = this.cnt || 0;
+    return this.cnt++;
+}
+
+function eventHandler(msg, callback) {
+    function onComplete() {
+        var error = Math.random() > 0.85;
+        callback(error, msg);
+    }
+    // processing takes time...
+    setTimeout(onComplete, Math.floor(Math.random() * 1000));
+}

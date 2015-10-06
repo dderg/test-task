4caaf56ce575ca570227000e40f89cef5fde64a0
Danila Dergachev <gnomdan@yandex.ru>
2015-10-06 16:35:01 +0300

    some generator checking stuff

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const redis = require('redis');
 const client = redis.createClient();
+const crypto = require('crypto');
 
 client.on('error', function (error) {
     console.error(error);
@@ -18,3 +21,25 @@ function eventHandler(msg, callback) {
     // processing takes time...
     setTimeout(onComplete, Math.floor(Math.random() * 1000));
 }
+
+let lastHex;
+function startGenerator() {
+    function updateHash() {
+         let sha = crypto.createHash('sha1');
+        sha.update(Math.random().toString());
+        sha.update(new Date().toString());
+        let hex = sha.digest('hex');
+        client.set('generatorHash', hex);
+    }
+    setInterval(() => {
+        updateHash();
+    }, 200);
+    updateHash();
+}
+
+setInterval(() => {
+    client.get('generatorHash', function (err, res) {
+        console.log(res === lastHex);
+        lastHex = res;
+    });
+}, 500);
